b3342367b700b3fc0f6bf0112388e58bf9cf44e1
ONeal Ombu <ombu.oneal@yahoo.com>
2023-08-24 00:08:42 +0100

    Set up routing in main.jsx for different views

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { HashRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./App.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <HashRouter hashType="hashbang">
+  <React.Fragment>
+    <Router>
       <App />
-    </HashRouter>
-  </React.StrictMode>
+    </Router>
+  </React.Fragment>
 );
