bac46bd447e2dffe3f7f64bfa3f16a8ebf1d5b38
ONeal Ombu <ombu.oneal@yahoo.com>
2023-08-23 11:31:13 +0100

    Modifed main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { HashRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 import "./App.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
