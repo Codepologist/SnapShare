a7a5aa201103955bb0d6884a0f41a2d9b00cf321
ONeal Ombu <ombu.oneal@yahoo.com>
2023-08-24 00:09:44 +0100

    Implement main structure in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,16 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/accounts/Login";
+import Home from "./container/Home";
+
 const App = () => {
   return (
-    <div>
-      <h1 className="title">Hello World!</h1>
-    </div>
+    <React.Fragment>
+      <Routes>
+        <Route path="accounts/login" element={<Login />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </React.Fragment>
   );
 };
 
