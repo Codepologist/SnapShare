93e83b333cd6e0e6c9c5325ba8316f3cc463563c
ONeal Ombu <ombu.oneal@yahoo.com>
2023-08-25 19:20:27 +0100

    Added Google0AuthProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./App";
 import "./App.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.Fragment>
     <Router>
-      <App />
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_PUBLIC_GOOGLE_API_TOKEN}>
+        <App />
+      </GoogleOAuthProvider>
     </Router>
   </React.Fragment>
 );
